54e86ffa15877844184b340d5034f4a6255ba3c9
Jacek Chmiel <jchmiel@collective-sense.com>
2018-05-15 21:21:27 +0200

    introduce pipenv

--- create_venv.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-virtualenv -p python3 venv
-source venv/bin/activate
-pip install -r requirements.txt
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='djedjbot',
-    scripts='djedjbot.py',
-    install_requires=['requests', 'spotipy'],
-    use_scm_version=True,
-    setup_requires=['setuptools_scm']
-)
